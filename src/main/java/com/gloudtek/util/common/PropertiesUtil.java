08c820450efdd39a12e9601255a01277f9dbd11c
MrRiver <631094182@qq.com>
2016-06-01 11:26:46 +0800

    改为jdk1.7+的写法

--- src/main/java/com/gloudtek/util/common/PropertiesUtil.java
+++ src/main/java/com/gloudtek/util/common/PropertiesUtil.java
@@ -1,39 +1,31 @@
 package com.gloudtek.util.common;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public class PropertiesUtil {
 
-	//根据key值读取配置文件的value值
-	public static String getProperties(String filePath,String key){
-		BufferedInputStream in = null;
-    	Properties p = new Properties(); 
-    	String value = null;
-		try {
-			in = new BufferedInputStream(new FileInputStream(filePath));
-			p.load(in);
-			value = p.getProperty(key);
-			if(value==null||value.length()==0||value.trim().equals("")){
-				System.out.println("文件内容为空或未找到相应的key("+key+")"+"值");
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("文件("+filePath+")不存在");
-		} catch (IOException e) {
-			System.out.println("IO异常");
-		} finally{
-			if(in!=null){
-				try {
-					in.close();
-				} catch (IOException e) {
-					System.out.println("流关闭失败");
-				}
-			}
+	//获取Properties对象
+	public static Properties getProperties(String filePath){
+		Properties properties = new Properties(); 
+		Path path = Paths.get(filePath);
+		try(InputStream inputStream = Files.newInputStream(path);){
+			properties.load(inputStream);
+		}catch(Exception e){
+			//do nothing
 		}
-		return value;
+		return properties;
+	}
+	
+	//根据key值获得value值
+	public static String getValueByKey(Properties properties,String key){
+		if(key==null){
+			return key;
+		}
+		return properties.getProperty(key);
 	}
 	
 }
